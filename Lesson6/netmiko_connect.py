b5036e9fbf5c57c4b9c45a5820b1c5d506230dfe
Boozinka <wayne.bellward@gmail.com>
2021-03-22 11:07:12 +0000

    Perform a basic Netmiko connect

---
+++ Lesson6/netmiko_connect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# SSH Connect Using Netmiko
+#
+# Makes an SSH connection using Netmiko
+
+# ConnectHandler
+from netmiko import Netmiko
+from getpass import getpass
+
+net_conn = Netmiko(host ='192.168.152.100', username='wbellward',
+                   password=getpass(), device_type='cisco_ios')
+
+print(net_conn.find_prompt())
+
